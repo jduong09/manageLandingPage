440f654219fcd944f83b160d70cb5dd2aeccf71b
Justin Duong <jduong@umich.edu>
2022-10-14 19:04:56 -0400

    Added 922px media query and started 1200px media query. Need to work on horizontal scrolling testimonials list.

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const iconCloseMenu = document.getElementById('nav-close-img');
   const listMenu = document.getElementById('list-header-menu');
   const bgMenu = document.getElementById('bg-menu');
+  const btnHeader = document.querySelector('header .btn-get-started');
 
   iconOpenMenu.addEventListener('click', () => {
     iconOpenMenu.classList.add('hide');
@@ -17,4 +18,34 @@ document.addEventListener('DOMContentLoaded', () => {
     listMenu.classList.add('hide');
     bgMenu.classList.add('hide');
   });
+
+  window.addEventListener('resize', () => {
+    if (innerWidth >= 1200) {
+      iconOpenMenu.classList.add('hide');
+      iconCloseMenu.classList.add('hide');
+      listMenu.classList.remove('hide');
+      bgMenu.classList.add('hide');
+      btnHeader.classList.remove('hide');
+    } else {
+      iconOpenMenu.classList.remove('hide');
+      iconCloseMenu.classList.add('hide');
+      listMenu.classList.add('hide');
+      bgMenu.classList.add('hide');
+      btnHeader.classList.add('hide');
+    }
+  });
+
+  if (innerWidth >= 1200) {
+    iconOpenMenu.classList.add('hide');
+    iconCloseMenu.classList.add('hide');
+    listMenu.classList.remove('hide');
+    bgMenu.classList.add('hide');
+    btnHeader.classList.remove('hide');
+  } else {
+    iconOpenMenu.classList.remove('hide');
+    iconCloseMenu.classList.add('hide');
+    listMenu.classList.add('hide');
+    bgMenu.classList.add('hide');
+    btnHeader.classList.add('hide');
+  }
 });
--- js/testimonials.js
+++ js/testimonials.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const itemTestimonials = document.getElementsByClassName('item-testimonial');
   const btnTestimonials = document.getElementsByClassName('btn-testimonial');
+  const divBtnTestimonials = document.getElementById('div-btn-testimonials');
   let currentTestimonialIdx = 0;
 
   for (let i = 0; i < btnTestimonials.length; i++) {
@@ -19,5 +20,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
       currentTestimonialIdx = i;
     });
+  }
+
+  for (let k = 0; k < itemTestimonials.length; k++) {
+    if (innerWidth >= 1200) {
+      itemTestimonials[k].classList.remove('hide');
+      divBtnTestimonials.classList.add('hide');
+    } else {
+      if (k === 0) {
+        itemTestimonials[k].classList.remove('hide');
+      } else {
+        itemTestimonials[k].classList.add('hide');
+      }
+    }
+
+    currentTestimonialIdx = 0;
   }
 });
